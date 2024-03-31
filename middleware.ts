55dd20bace858540e2e928b05d8d92d90632e156
Mladen Antic <mladen.antic98@gmail.com>
2024-03-31 13:21:10 +0200

    Updating middleware

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,14 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: [
-    "/",
+    // "/",
     "/api/webhooks(.*)",
-    "/question/:id",
-    "/tags",
-    "/tags/:id",
-    "/profile/:id",
-    "/community",
-    "/jobs"
+    // "/question/:id",
+    // "/tags",
+    // "/tags/:id",
+    // "/profile/:id",
+    // "/community",
+    // "/jobs"
   ],
   // ignoredRoutes: ["/api/webhooks(.*)", "/api/chatgpt"],
 });
