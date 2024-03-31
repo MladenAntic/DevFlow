329694e7831522c6bb43d7df12ba101d9ff22418
Mladen Antic <mladen.antic98@gmail.com>
2024-03-31 18:55:26 +0200

    Creating Tags page, displaying current tags

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,8 +1,9 @@
-"use server"
+"use server";
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -12,12 +13,28 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
 
     const user = await User.findById(userId);
 
-    if(!user) throw new Error("User not found");
+    if (!user) throw new Error("User not found");
 
     // Find interactions for the user and group by tags...
     // Interaction...
 
-    return [ {_id: '1', name: 'tag1'}, {_id: '2', name: 'tag2'}]
+    return [
+      { _id: "1", name: "tag1" },
+      { _id: "2", name: "tag2" },
+    ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
