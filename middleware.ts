939a3bb96e63d4399b73fbb4d839074a076be7e7
Mladen Antic <mladen.antic98@gmail.com>
2024-04-08 12:23:13 +0200

    Updating middleware, fixing clerk modal position

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/api/webhooks/clerk",
-    "/question/:id",
+    "question/:id",
     "/tags",
     "/tags/:id",
     "/profile/:id",
